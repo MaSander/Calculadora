798143d88ede848acbbc444ae7492f036fe3a3ba
unknown <ma.sander11@gmail.com>
2020-08-09 14:27:42 -0300

    inserção de decimais

--- cerebrum.js
+++ cerebrum.js
@@ -1,34 +1,51 @@
-var currentHistory
 var numbers = []
 var commands = []
+var result = 0
 
 function func(event) {
-    numbers[0] = document.getElementById("input").value += event;
-    console.log(numbers);
-    console.log(numbers.length);
-    console.log(commands.length);
+    numbers[commands.length] = result = document.getElementById("input").value += event;
+    console.log(numbers)
 }
 
-function ops(event) {
-    if(numbers.length == commands.length){
-        commands[commands.length] = event;
-        return;
+function operations(event) {
+    if(commands.length == numbers.length){
+        commands[commands.length - 1] = event;
     }
-    commands[commands.length] = event;
-    console.log(commands);
+    else
+    {
+        commands[commands.length] = event;
+    }
+    document.getElementById("input").value = ""
+    console.log(commands)
 }
 
-function comma() {
-    //insert comma in number
+function score() {
+    numbers[numbers.length - 1] = numbers[numbers.length - 1] + "."
+    console.log((numbers.length - 1) + " number in array")
+    document.getElementById("input").value = numbers[numbers.length - 1]
 }
 
 function equal() {
-    for(var item in history){
-        console.log(commandsHistory)
+    for(var i = 0; i < numbers.length; i++ ){
+        switch (commands[i]){
+            case "multiplicator":
+                result = parseFloat(numbers[i]) * parseFloat(numbers[i + 1])
+                break;
+            case "subtract":
+                result = parseFloat(numbers[i]) - parseFloat(numbers[i + 1])
+                break;
+            case "sum":
+                result = parseFloat(numbers[i]) + parseFloat(numbers[i + 1])
+                break;
+        }
     }
+    document.getElementById("input").value = result
+    numbers = [result]
+    commands = []
 }
 
-function clear() {
-    currentHistory = null
-    document.getElementById("input").innerHTML = ''
+function toClear() {
+    document.getElementById("input").value = ""
+    numbers = []
+    commands = []
 }
